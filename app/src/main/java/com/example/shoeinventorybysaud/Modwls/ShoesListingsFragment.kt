e1960a7b41c3ddabc8854f35fb5effded3dafa10
Saud5-0-7 <vgn714@gmail.com>
2021-09-15 03:12:03 +0300

    ShoeInventoryBySaud

--- app/src/main/java/com/example/shoeinventorybysaud/Modwls/ShoesListingsFragment.kt
+++ app/src/main/java/com/example/shoeinventorybysaud/Modwls/ShoesListingsFragment.kt
@@ -42,20 +42,21 @@ class ShoesListingsFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    inflater.inflate(R.menu.logout, menu)
+    inflater.inflate(R.menu.main_menu, menu)
 }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return when (item.itemId) {
-        R.id.loginFragment -> {
+        return when (item.itemId) {
+            R.id.loginFragment-> {
 
-            true
+                true
 
 
+            }
+            else -> onOptionsItemSelected(item)
         }
-        else -> onOptionsItemSelected(item)
     }
 }
 
