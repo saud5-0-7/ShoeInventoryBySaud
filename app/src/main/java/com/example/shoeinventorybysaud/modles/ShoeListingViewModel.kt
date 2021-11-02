084b0bfede397d1af7457fded251615a8bc26d7a
Saud5-0-7 <vgn714@gmail.com>
2021-11-02 18:01:04 +0300

    ShoeInventoryBySaud

--- app/src/main/java/com/example/shoeinventorybysaud/modles/ShoeListingViewModel.kt
+++ app/src/main/java/com/example/shoeinventorybysaud/modles/ShoeListingViewModel.kt
@@ -1,17 +1,38 @@
 
 package com.example.shoeinventorybysaud.modles
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.shoeinventorybysaud.BR
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class ShoeListingViewModel: ViewModel() {
+class ShoeListingViewModel: ViewModel(), Observable() {
 
 
     val shoeList = MutableLiveData<MutableList<Shoe>>()
 
     init {
         shoeList.value = ArrayList()
+    }
+
+
+    @Bindable
+    var shoe = Shoe()
+        set(value) {
+            if(value != field) {
+                field = value
+
+                saveData()
+
+                notifyPropertyChanged(BR.ShoeListingViewModel)
+            }
+        }
+
+    private fun saveData() {
+        TODO("Not yet implemented")
     }
 
     fun addShoe(name: String, size: String, company: String, description: String) {
@@ -22,14 +43,10 @@ class ShoeListingViewModel: ViewModel() {
 
     }
 
-    fun addDummyData(){
-        addShoe("", "9", "Adidas", "Great shoes")
-        addShoe("Pumaaaaaaaa", "9", "Adidas", "Great shoes")
-
     }
 
 
-}
+
 
 
 
